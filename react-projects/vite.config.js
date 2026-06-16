dd47ca1cb0e5e34af985b277bfbcdb4074112093
Timothy Cabello <timgc@Mac-Studio.local>
2026-06-15 20:02:33 -0600

    Fixed: build react app to dist, and add launch button on the projects page

--- react-projects/vite.config.js
+++ react-projects/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-projects/dist/',
   build: {
-    outDir: "../pages/react-dist",
-    emptyOutDir: true,
-  },
-});
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
